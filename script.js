3cccc38c67c9889d76e2ae516e0cf3e223c79679
KJFreys1 <kfreyermuth20@gmail.com>
2020-03-09 17:13:22 -0700

    added modals and temp project

---
+++ script.js
@@ -0,0 +1,34 @@
+const modalOne = document.querySelector(".modal-one")
+const modalTwo = document.querySelector(".modal-two")
+const modalThree = document.querySelector(".modal-three")
+const closeOne = document.querySelector(".c_one")
+const closeTwo = document.querySelector(".c_two")
+const closeThree = document.querySelector(".c_three")
+
+closeOne.addEventListener("click", () => {
+    modalOne.style.display = "none"
+})
+
+closeTwo.addEventListener("click", () => {
+    modalTwo.style.display = "none"
+})
+
+closeThree.addEventListener("click", () => {
+    modalThree.style.display = "none"
+})
+
+const devTickets = document.querySelector("#DevTickets")
+const devSync = document.querySelector("#DevSync")
+const thoughtShower = document.querySelector("#Thought-Shower")
+
+devTickets.addEventListener("click", () => {
+    modalOne.style.display = "block"
+})
+
+devSync.addEventListener("click", () => {
+    modalTwo.style.display = "block"
+})
+
+thoughtShower.addEventListener("click", () => {
+    modalThree.style.display = "block"
+})
